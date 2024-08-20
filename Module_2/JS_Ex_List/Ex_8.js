89be0aaef639d6644a052342e30d5f7df3d35ad0
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2024-08-19 19:00:54 -0600

    done ex_9

--- Module_2/JS_Ex_List/Ex_8.js
+++ Module_2/JS_Ex_List/Ex_8.js
@@ -1,3 +1,14 @@
 /* EJERCICIO 8: 
 Escriba un programa que muestre la tabla de multiplicar del 1 al 10 del número ingresado 
 por el usuario */
+
+const randomInput = Math.floor(Math.random() * 10 + 1);
+console.log(`\nInput del usuario: ${randomInput}`);
+
+let count = 1;
+
+while (count <= 10) {
+  let tabla = randomInput * count;
+  console.log(`${count} X ${randomInput} = ${tabla} `);
+  count++;
+}
--- Module_2/JS_Ex_List/Ex_9.js
+++ Module_2/JS_Ex_List/Ex_9.js
@@ -5,3 +5,23 @@ Fahrenheit, una vez teniendo la temperatura en Fahrenheit deberá devolver lo si
 • Si ºF está entre 32 y 68, sale la frase “Temperatura adecuada”
 • Si ºF está entre 68 y 96, sale la frase “Temperatura alta”
 • Si no está entre ningún caso anterior la frase “Temperatura desconocida” */
+
+const tempC = Math.floor(Math.random() * 60 + 1);
+
+const CtoF = (celsius) => {
+  return (celsius * 9) / 5 + 32;
+};
+const tempF = CtoF(tempC);
+
+console.log("\nC°: " + tempC);
+console.log("F°: " + tempF);
+
+if ((tempF >= 14) & (tempF < 32)) {
+  console.log("Temperatura baja");
+} else if ((tempF >= 32) & (tempF < 68)) {
+  console.log("Temperatura adecuada");
+} else if ((tempF >= 68) & (tempF < 96)) {
+  console.log("Temperatura alta");
+} else {
+  console.log("Temperatura desconocida");
+}
